6b83f2c7f8ea981c75f46050054e5785b84cba0f
Roman Romanov <rromanov.vk@gmail.com>
2025-12-04 14:07:17 +0500

    init

---
+++ SwiftfulThinkingBootcampContinuation/SwiftfulThinkingBootcampContinuationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampContinuationApp.swift
+//  SwiftfulThinkingBootcampContinuation
+//
+//  Created by Roman Romanov on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampContinuationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
