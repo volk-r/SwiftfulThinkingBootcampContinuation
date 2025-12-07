a96204ddcd430aeb3897b59e8ded01417a712012
Roman Romanov <rromanov.vk@gmail.com>
2025-12-07 21:47:19 +0500

    Hashable

---
+++ SwiftfulThinkingBootcampContinuation/HashableBootcamp.swift
@@ -0,0 +1,42 @@
+//
+//  HashableBootcamp.swift
+//  SwiftfulThinkingBootcampContinuation
+//
+//  Created by Roman Romanov on 07.12.2025.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+	let title: String
+
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(title)
+	}
+}
+
+struct HashableBootcamp: View {
+
+	let data: [MyCustomModel] = [
+		MyCustomModel(title: "ONE"),
+		MyCustomModel(title: "TWO"),
+		MyCustomModel(title: "THREE"),
+		MyCustomModel(title: "FOUR"),
+		MyCustomModel(title: "FIVE")
+	]
+
+    var body: some View {
+		ScrollView {
+			VStack(spacing: 40) {
+				ForEach(data, id: \.self) { item in
+					Text(item.title)
+						.font(.headline)
+				}
+			}
+		}
+    }
+}
+
+#Preview {
+    HashableBootcamp()
+}
